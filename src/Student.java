09c7bb8913ab2337c2870ccc93292437ca26a91d
octak <92293768+octak@users.noreply.github.com>
2021-11-19 11:31:46 +0000

    delete all

--- src/Student.java
+++
@@ -1,8 +0,0 @@
-import java.text.MessageFormat;
-
-public record Student(String identifier, String name, String id) {
-    @Override
-    public String toString() {
-        return MessageFormat.format("[{0}, {1}, {2}]", identifier, name, id);
-    }
-}
